bdd4231c8c198e86fbcea49b0aef64167f3e40b1
Jae0 <leey153@naver.com>
2026-07-03 22:34:52 +0900

    🏗️ chore: shared/api 배럴에 getApiErrorCode export 추가

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,3 +1,4 @@
+export { getApiErrorCode } from './errors/getApiErrorCode';
 export { auth } from './firebase/firebase';
 export { httpClient } from './http/httpClient';
 export type { APIResponse, ApiErrorBody, CustomQueryOptions } from './types';
