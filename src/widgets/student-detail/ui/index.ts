44f7aad0ebf86b1ca4697a47da26e3e42a4eda56
Jae0 <leey153@naver.com>
2026-07-12 17:41:18 +0900

    🏗️ chore: widgets/student-detail/ui 배럴에 PaymentHistory·PaymentHistorySkeleton·PaymentSummaryCard·PaymentSummarySkeleton export 추가

--- src/widgets/student-detail/ui/index.ts
+++ src/widgets/student-detail/ui/index.ts
@@ -1,2 +1,6 @@
+export * from './PaymentHistory';
+export * from './PaymentHistorySkeleton';
 export * from './PaymentRow';
+export * from './PaymentSummaryCard';
+export * from './PaymentSummarySkeleton';
 export * from './StudentDetail';
